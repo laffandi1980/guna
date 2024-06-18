3062a1755c98d3029e6665db285d97d7723c4e2a
laffandi1980 <dianaffandi@gmail.com>
2024-06-18 19:32:35 +0700

    yg login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use GroceryCrud\Core\Validate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -24,6 +26,10 @@ class LoginController extends Controller
             'password' => $request->password,
         ];
         if(Auth::attempt($data)){
+            $post = DB::select('SELECT * FROM users where email=?',array($request->email));
+            foreach($post as $row){
+                Session::put('name',$row->name);
+            }
             return redirect()->route('dashboard');
         }else{
             return redirect()->route('login')->with('failed','Periksa email atau password Anda!');
--- resources/views/default_template1.blade.php
+++ resources/views/default_template1.blade.php
@@ -191,9 +191,9 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Session::get('name')}}</span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="dashboard1/img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -278,10 +278,10 @@
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Pilih Logout untuk mengakhiri sesi ini!</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="{{url('login')}}">Logout</a>
                 </div>
             </div>
         </div>
